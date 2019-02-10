c9721af62f91ea8272980f1d1bb160dca1621bd0
Shocewin <Shocewin@yandex.ru>
2019-02-10 16:10:14 +0300

    test parser implementation in main function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using YetAnotherPreprocessorCss.LexicalAnalysis;
 
 namespace YetAnotherPreprocessorCss
 {
@@ -10,6 +8,37 @@ namespace YetAnotherPreprocessorCss
 	{
 		static void Main(string[] args)
 		{
+			string path = args[0];
+			path += ".yapc";
+			Lexer lex = new Lexer(path);
+			FileInfo fi = new FileInfo(path);
+			path = $"{fi.DirectoryName}\\{fi.Name.Replace(".yapc", ".css")}";
+			StreamWriter sw = File.CreateText(path);
+			bool openSelector = false;
+			while(true)
+			{
+				IToken token = lex.Scan();
+				if (token is EOF) break;
+				if (token is Selector)
+				{
+					if (openSelector)
+					{
+						sw.WriteLine("}");
+						sw.WriteLine($"{(token as Selector).Name} {{");
+						openSelector = true;
+					}
+					else
+					{
+						sw.WriteLine($"{(token as Selector).Name} {{");
+						openSelector = true;
+					}
+				}
+				if (token is EndSelector) openSelector = false;
+				if (token is Property) sw.WriteLine($"\t{(token as Property).Name} : {(token as Property).Value};");
+			}
+			sw.WriteLine("}");
+			sw.Close();
+			Console.WriteLine("Completed");
 		}
 	}
 }
